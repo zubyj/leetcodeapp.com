d8fc336657c91add19d5733d7663e82419264fee
Zuby Javed <zubyjaved@gmail.com>
2023-07-24 12:54:37 -0700

    add firefox and edge download links " git push

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const titleEl = document.getElementById('title');
 titleEl.classList.add('animate');
+
+window.onload = function () {
+    let cards = document.getElementsByClassName("card");
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].onclick = function () {
+            this.querySelector('.btn').click();
+        };
+    }
+};
