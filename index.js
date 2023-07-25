92d3dfc94fb62e0603cc7f38d2ed298a0a793f22
Zuby Javed <zubyjaved@gmail.com>
2023-07-25 13:19:38 -0700

    cleanup code & make feature cards more responsive on medium devices

--- index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    let cards = document.getElementsByClassName("card");
-
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].onclick = function () {
-            this.querySelector('.btn').click();
-        };
-    }
-};
